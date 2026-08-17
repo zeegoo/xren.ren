a2a2d726438a56bb92602ec89f44172963a32d09
zhouge <zhouge@zhouges-iMac.local>
2026-08-17 23:14:18 +0800

    chore: add .gitignore, remove cache/dist from tracking

--- xren/docs/.vitepress/dist/assets/chunks/swimlanesDiagram-ULZ7WXOC.Mry2lnCM.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DgF04Sw4.js";import{_ as a}from"../app.B3naSO3g.js";import"./chunk-5VM5RSS4.BV82TVOg.js";import"./chunk-XXDRQBXY.C54DG089.js";import"./chunk-KBJHAD2P.CRmLQpii.js";import"./chunk-2GRJ4B5K.CbNUsAEA.js";import"./channel.BsOOurQ7.js";import"./framework.BHRys-F3.js";import"./theme.CgxWUmWe.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
